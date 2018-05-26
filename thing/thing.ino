2e8da8ed026d141f8dd034ec804c15e729b55174
Almir M. da Cunha <almirmdacunha@gmail.com>
2018-05-26 18:01:25 -0300

    Setup thing

--- thing/thing.ino
+++ thing/thing.ino
@@ -1,4 +1,5 @@
 #include <KNoTThing.h>
+#include <EmonLib.h>
 
 #define CT_ID 1
 #define CT_PIN 1
@@ -9,11 +10,33 @@
 #define RELAY_NAME "Relay"
 
 KNoTThing thing;
+EnergyMonitor emon1;
+
+static int current_read(uint8_t *val) {
+  return 0;
+}
+
+static int relay_read(uint8_t *val) {
+ return 0;
+}
+
+static int relay_write(uint8_t *val) {
+  return 0;
+}
 
 void setup() {
   Serial.begin(9600);
+  
   pinMode(RELAY_PIN, OUTPUT);
+  emon1.current(CT_PIN, 111.1); 
+  
   thing.init("KNoTThing");
+  thing.registerFloatData(CT_NAME, CT_ID, KNOT_TYPE_ID_CURRENT, KNOT_UNIT_CURRENT_A, current_read, );
+  thing.registerBoolData(RELAY_NAME, RELAY_ID, KNOT_TYPE_ID_SWITCH, KNOT_UNIT_NOT_APPLICABLE, relay_read, relay_write);
+  thing.registerDefaultConfig(CT_ID, KNOT_EVT_FLAG_CHANGE, 0, 0, 0, 0, 0);
+  thing.registerDefaultConfig(RELAY_ID, KNOT_EVT_FLAG_TIM, 10, 0, 0, 0, 0);
+
+  Serial.println(F("Perkw Thing"));
 }
 
 void loop() {
