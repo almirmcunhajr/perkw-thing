77b009992435728911218d8cb86e0786e9d9c1c9
Almir M. da Cunha <almirmdacunha@gmail.com>
2018-05-26 17:20:23 -0300

    Define IDs and PINs and init thing

---
+++ thing/thing.ino
@@ -0,0 +1,21 @@
+#include <KNoTThing.h>
+
+#define CT_ID 1
+#define CT_PIN 1
+#define CT_NAME "Current transformer"
+
+#define RELAY_ID 2
+#define RELAY_PIN 1
+#define RELAY_NAME "Relay"
+
+KNoTThing thing;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(RELAY_PIN, OUTPUT);
+  thing.init("KNoTThing");
+}
+
+void loop() {
+  thing.run();
+}
